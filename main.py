5be847c92e3cedb504963378154609b392d51982
None <sullome@yandex.com>
2015-02-28 19:58:20 +0300

    Start translation.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 import os.path as path
 import xml.etree.ElementTree as ET
 from random import choice, randint
+import gettext
 from jang.main import single_name
+
+gettext.install('charfly', '/home/none/prj/charfly/locale')
 
 split_words = lambda s: s.capitalize().translate(str.maketrans('_', ' '))
 
@@ -23,8 +26,8 @@ def xml2opt(path):
     root = tree.getroot()
 
     for type_ in root:
-        tag = split_words(type_.tag)
-        txt = choice(list(type_)).text
+        tag = split_words(_(type_.tag))
+        txt = _(choice(list(type_)).text)
         result.append('{}: {}'.format(tag, txt))
 
     emph = choice(result)
@@ -44,15 +47,16 @@ def create(nation, gender, mid_age, datapath = get_data()):
     return (name, age, mannerism, m_other, first_impression, f_other)
 
 def decorate(char):
-    return '''{0[0]} ({0[1]})
-
-{0[2]}
-Other:
-    {1}
-
-{0[4]}
-Other:
-    {2}'''.format(char, '\n    '.join(char[3]), '\n    '.join(char[5])) 
+    return ('{0[0]} ({0[1]})'
+        '\n\n{0[2]}'
+        '\n{3}:\n    {1}'
+        '\n\n{0[4]}'
+        '\n{3}:\n    {2}').format(
+            char,
+            '\n    '.join(char[3]),
+            '\n    '.join(char[5]),
+            _('Other')
+        ) 
 
 def main ():
     character = create('eng', 'male', 30, '/home/none/prj/charfly')
