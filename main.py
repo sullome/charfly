5e2bbe84bfc03abfc3969a513cd79e7a64c0b79b
None <sullome@yandex.com>
2015-03-12 05:18:56 +0300

    Add first reaction. Need testing.

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ split_words = lambda s: s.capitalize().translate(str.maketrans('_', ' '))
 def get_data():
     return '/home/none/prj/charfly'
 
-def get_age(mid):
-    # mid + 10* (4df) + d10
-    age = 0
-    while age <= 0:
-        age = mid + sum(choice([-10, 0, 10]) for i in range(4)) + randint(1,10)
-    return age
-
 def xml2opt(path):
     result = []
 
@@ -35,6 +28,24 @@ def xml2opt(path):
 
     return (emph, result)
 
+def get_age(mid):
+    # mid + 10 * (4df) + d10
+    age = 0
+    while age <= 0:
+        age = mid + sum(choice([-10, 0, 10]) for i in range(4)) + randint(1,10)
+    return age
+
+def get_first_reaction():
+    tree = ET.parse('first-reactions.xml')
+    root = tree.getroot()
+
+    roll = randint(1,6) + randint(1,6)
+
+    for reaction in root:
+        if roll in range(int(reaction.get('start')), int(reaction.get('end')) + 1):
+            tag = split_words(_(reaction.tag))
+            return (reaction.tag, reaction.text)
+
 def create(nation, gender, mid_age, datapath = get_data()):
     name = single_name(nation, gender)
     age = get_age(mid_age)
@@ -44,9 +55,11 @@ def create(nation, gender, mid_age, datapath = get_data()):
     first_impression, f_other = xml2opt(
         path.join(datapath, 'first-impressions.xml')
     )
+    first_reaction = get_first_reaction()
     return (name, age, nation, gender,
             mannerism, first_impression,
-            m_other, f_other)
+            m_other, f_other,
+            first_reaction)
 
 def create_decorated(nation, gender, mid_age, count, datapath = get_data()):
     if count < 1:
@@ -70,13 +83,16 @@ def decorate(char, full = True):
             '{3}: '
             '{0[4][1]} ({0[4][0]})\n'
             '{4}: '
-            '{0[5][1]} ({0[5][0]})'
+            '{0[5][1]} ({0[5][0]})\n'
+            '{5}: '
+            '{0[8][0]}'
             ).format(
                 char,
                 _('Nation'),
                 _('gender'),
                 _('Mannerism'),
-                _('First impression')
+                _('First impression'),
+                _('First reaction')
                 )
     else:
         return (
@@ -87,13 +103,17 @@ def decorate(char, full = True):
             '{1}\n\n'
             '{0[5][0]}: {0[5][1]}\n'
             '{5}:\n    '
-            '{2}').format(
+            '{2}\n\n'
+            '{6}: '
+            '{0[8][0]}\n{0[8][1]}'
+            ).format(
                 char,
                 '\n    '.join('{0[0]}: {0[1]}'.format(t) for t in char[6]),
                 '\n    '.join('{0[0]}: {0[1]}'.format(t) for t in char[7]),
                 _('Nation'),
                 _('gender'),
-                _('Other')
+                _('Other'),
+                _('First reaction')
                 ) 
 
 def main ():
@@ -101,4 +121,5 @@ def main ():
     print(decorate(character))
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print(get_first_reaction())
