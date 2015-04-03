d14d51ff8e60b95f352065019be36d96cca97ed2
None <sullome@yandex.com>
2015-04-03 21:45:45 +0300

    Fix class name in main().

--- gtk3.py
+++ gtk3.py
@@ -136,7 +136,7 @@ class CharflyGtk(Gtk.Window):
         return None
 
 def main():
-    win = FastCharacter()
+    win = CharflyGtk()
     win.connect('delete-event', Gtk.main_quit)
     win.show_all()
     Gtk.main()
